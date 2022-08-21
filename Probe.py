ffc88af8ad7fe45dd77f1a77c46db35db90a382a
gbrs <gbrs@mail.ru>
2022-08-21 11:29:22 +0300

    try to solve 1.9.3

--- Probe.py
+++ Probe.py
@@ -1,22 +1,5 @@
-post_fix = []
-st = ['(']
-
-for w in '7 * 8 + ( 3 - 5 )'.split() + [')']:
-    if w.isnumeric():
-        post_fix.append(w)
-    if w == '(':
-        st.append(w)
-    elif w == ')':
-        a = st.pop()
-        while a != '(':
-            post_fix.append(a)
-            a = st.pop()
-    elif w in {'+', '-'}:
-        while st[-1] != '(':
-            post_fix.append(st.pop())
-        st.append(w)
-    elif w in {'*', '/'}:
-        if st[-1] in {'*', '/'}:
-            post_fix.append(st.pop())
-        st.append(w)
-print(*post_fix)
+for i in range(2 ** 4):
+    for j in range(4):
+        for c in (0, 1):
+            print(c, end='')
+    print()
